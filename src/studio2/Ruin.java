b85c6793bcc31b2461f1da1e3794d416689a4f3f
bradley <bradley@localhost>
2024-01-31 14:18:40 -0600

    It works when we win, but goes forever if we lose

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,46 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter starting money");
+		double startAmount = in.nextDouble();
+		System.out.println("Please enter your win limit");
+		double winLimit = in.nextDouble();
+		System.out.println("Please enter a number between 0 and 100 for your win percentage");
+		double winChance = in.nextDouble();
+		double ruinAmount = 0.00;
+		while (startAmount > ruinAmount)
+		{
+		while (startAmount < winLimit)
+		{
+		double randomNumber = Math.random() * 100;
+		
+		if (winChance > randomNumber)
+			{
+			startAmount = startAmount + 1;
+			System.out.println("You won! You now have $" + startAmount);
+				if (startAmount == winLimit)
+				{
+				System.out.println("You win and hit the win limit! You now have $" + winLimit);
+				}
+			}
+		else
+			{
+			startAmount = startAmount - 1;
+			System.out.println("You lost. You now have $" + startAmount);
+				if (startAmount <= ruinAmount)
+				{
+				System.out.println("You lost all of your money. Don't gamble idiot.");
+				}
+			}
+		}
+		}
+	}
+}
+		
+
